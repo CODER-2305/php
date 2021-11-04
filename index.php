83e70b7c39bddf3981713a95b2c0eeaf55a5d3b5
CODER-2305 <Misterhayrulllo@gmail.com>
2021-11-04 20:04:37 +0500

    first

---
+++ index.php
@@ -0,0 +1,3 @@
+<?
+include_once('./pages/basic.php');
+?>
---
+++ pages/basic.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic</title>
+    <link rel="stylesheet" href="./components/css/style.css?v=<?= time() ?>">
+</head>
+<body>
+<nav>
+            <label class="logo">Scode.uz</label>
+            <ul>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Follow</a></li>
+            </ul>
+            <label id="icon">
+                <i class="fas fa-bars"></i>
+            </label>
+        </nav>
+        <div class="main">
+        <div class="container">
+            <div class="typer">
+                <h1>Helllo Welcome to S'code</h1>
+                <h2>we are a
+                    <div class="animate-text">
+                        <span>Freelencer</span>
+                        <span>Developer</span>
+                        <span>Desinger</span>
+                        <span>Fullstack</span>
+                        <span>Cyber__off</span>
+                    </div>
+                </h2>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="container">
+            <h2>Programming Language on Scode</h2>
+            <div class="skills">
+                <span class="name">Html</span>
+                <div class="percent">
+                    <div class="progress" style="width:95%;"></div>
+                </div>
+                <span class="value">95%</span>
+            </div>
+            <div class="skills">
+                <span class="name">Css</span>
+                <div class="percent">
+                    <div class="progress" style="width:90%;"></div>
+                </div>
+                <span class="value">90%</span>
+            </div>
+            <div class="skills">
+                <span class="name">Javascript</span>
+                <div class="percent">
+                    <div class="progress" style="width:87%;"></div>
+                </div>
+                <span class="value">87%</span>
+            </div>
+            <div class="skills">
+                <span class="name">Php</span>
+                <div class="percent">
+                    <div class="progress" style="width:70%;"></div>
+                </div>
+                <span class="value">70%</span>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
